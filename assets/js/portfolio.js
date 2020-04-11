0668cec16ccda815ebea2a7ad48ef48be13ec650
EricVincitore <epv1001@verizon.net>
2020-04-10 21:32:04 -0400

    updated all projects to new portfolio format.

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -30,3 +30,34 @@ $(".gifBtn").click(function() {
     $(".gitBtn").attr("href", "https://github.com/EricVincitore/Gif-App")
 });
 
+$(".yodaBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("Yoda Meme Generator")
+    $(".modal-body").html("Entertain yourself by creating memes after you convert text to the way Yoda speaks and add it to an image. This app was developed using HTML5, Bootstrap, jQuery, Rest API and Firebase")
+    $(".appBtn").attr("href", "https://adamm285.github.io/YodaMemeGenerator/")
+    $(".gitBtn").attr("href", "https://github.com/Adamm285/YodaMemeGenerator")
+});
+
+$(".friendBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("Friend Finder")
+    $(".modal-body").html("Take a short quiz and find a friend based off of your answers. This app was developed using HTML5, Bootstrap, jQuery, Express, Node and Path.")
+    $(".appBtn").attr("href", "https://friendfinderapp1001.herokuapp.com/")
+    $(".gitBtn").attr("href", "https://github.com/EricVincitore/Friend-Finder")
+});
+
+$(".burgerBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("Burger Paradise")
+    $(".modal-body").html("You are both the chef and customer in this app. Add burgers to the menu, devour them then take them off the menu as you please. This app was developed using HTML5, Bootstrap, jQuery, Express, Node, Express, Handlebars, MySql and Sequelize.")
+    $(".appBtn").attr("href", "https://burgerapp2epv1001.herokuapp.com/")
+    $(".gitBtn").attr("href", "https://github.com/EricVincitore/burger-the-sequel")
+});
+
+$(".aniBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("AniYou!")
+    $(".modal-body").html("Stay current with your favorite anime shows and movies. Search different anime and get information over the show or movie. This app was developed using HTML5, Bootstrap, Bootswatch, jQuery, Express, Node, Express, Handlebars, Passport, Path, MySql and Sequelize.")
+    $(".appBtn").attr("href", "https://aniyou1.herokuapp.com/")
+    $(".gitBtn").attr("href", "https://github.com/jwescott/AniYou")
+});
