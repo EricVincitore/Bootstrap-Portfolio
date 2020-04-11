6896c36c5023ebbeac62f8fdc1a764bc7108ae8f
EricVincitore <epv1001@verizon.net>
2020-04-10 20:41:40 -0400

    added fully functional modal for portfolio apps making jquery evetns for all projects

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,5 +1,15 @@
-$("#CCLabel").hover( function () {
-    
-}, function () {
-    $("#CCLabel").text("Command Central")
+$(".CCBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("Command Central")
+    $(".modal-body").html("An all encompassing Magic: the Gathering companion app that offers the user articles, metagame data, a card database and more. This app was developed using React, Reactstrap, Javascript, Node, Passport, Express and Mongoose.")
+    $(".appBtn").attr("href", "https://command-central.herokuapp.com/")
+    $(".gitBtn").attr("href", "https://github.com/EricVincitore/command-central")
+});
+
+$(".fishBtn").click(function() {
+    event.preventDefault();
+    $(".modal-title").html("MTG Data Fish")
+    $(".modal-body").html("Get the recent articles form MtgGoldfish to stay up to date while being able to leave comments on specific articles. This app was developed using Handlebars, Javascript, Node, Bootstrap, Express, and Mongoose.")
+    $(".appBtn").attr("href", "https://mtgdatafish.herokuapp.com/")
+    $(".gitBtn").attr("href", "https://github.com/EricVincitore/mongoose-hw")
 });
